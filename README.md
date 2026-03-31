adf251db0def198b8f62a2e0b367554127bf5a21
Sky <skylarplayz348@gmail.com>
2026-03-30 21:20:02 -0500

    Start Project
    See CHANGELOG

---
+++ README.md
@@ -0,0 +1,7 @@
+# svDoom
+My DOOM Source Port because why not.
+
+## Why
+I just said it because I want to.
+
+Are there better port? Yes. Are You going to aim to be them? No. Will this be popular? Maybe. How should I know
--- linuxdoom-1.10/m_bbox.h
+++ linuxdoom-1.10/m_bbox.h
@@ -1,4 +1,4 @@
-// Emacs style mode select   -*- C++ -*- 
+// Emacs style mode select   -*- C++ -*-
 //-----------------------------------------------------------------------------
 //
 // $Id:$
@@ -16,17 +16,16 @@
 //
 // DESCRIPTION:
 //    Nil.
-//    
+//
 //-----------------------------------------------------------------------------
-
 
 #ifndef __M_BBOX__
 #define __M_BBOX__
 
-#include <values.h>
+#include <limits.h>
+#include <float.h>
 
 #include "m_fixed.h"
-
 
 // Bounding box coordinate storage.
 enum
@@ -35,17 +34,14 @@ enum
     BOXBOTTOM,
     BOXLEFT,
     BOXRIGHT
-};	// bbox coordinates
+}; // bbox coordinates
 
 // Bounding box functions.
-void M_ClearBox (fixed_t*	box);
+void M_ClearBox(fixed_t *box);
 
-void
-M_AddToBox
-( fixed_t*	box,
-  fixed_t	x,
-  fixed_t	y );
-
+void M_AddToBox(fixed_t *box,
+                fixed_t x,
+                fixed_t y);
 
 #endif
 //-----------------------------------------------------------------------------
